45770edb31aae7917ac6d6c75283fbb4ff676f36
Ange-237 <angeessome10@gmail.com>
2025-02-18 23:40:41 +0100

    Add files via upload

--- Jeux.py
+++ Jeux.py
@@ -17,7 +17,9 @@
         
 #         if keyboard.read_key() == "enter":  # Attendre la touche "Entrée"
 #             print()
-#             afficher_les_messages_lentement(f"Veuillez choisir votre personnage.")
+#             afficher_les_messages_lentement("combein de hero votn combattre ")
+#             afficher_les_messages_lentement("Veuillez choisir votre personnage.")
+#             
 #             break  # Sortir de la boucle après avoir pressé 'Entrée'
 
 # message_de_bienvenue() 
@@ -29,9 +31,12 @@
 
 
 class creature :#creation de la classe creature 
+     type_de_degat = ["contontdant","trachat","percant","Feu","poison","magique"]
+     etat = ["empoisonne","paralyse","inspiré"]
+     arme = ["lame maudite","katana","sabre","zanpakto"]
 
 #attributs
-    def __init__(self,name,descriptiton,pv,defense,initiative,attaque,type_de_degat=["contontdant","trachat","percant","Feu","poison","magique"],etat=["empoisonne","paralyse","inspiré"]) :
+def __init__(self,name,descriptiton,pv,defense,initiative,attaque,type_de_degat,etat) :
 
         self.name = name
         self.description = descriptiton
@@ -44,36 +49,30 @@ class creature :#creation de la classe creature
     # def lancer_linitiative() : 
 
     # def afficher_leurs_actions() :
-    
-    def afficher_leurs_caracteristiques(self) :
-
-        print(f"[{self.name},{self.description},{self.pv},{self.initiative},{self.defense},{self.etat}]")
-
-
-
+def afficher_leurs_caracteristiques(self) :
+    print(f"[{self.name},{self.description},{self.pv},{self.initiative},{self.defense},{self.etat}]")
 
 
 class personnage(creature): #classe personnage ou heros
  arme = ""
 inventaire = ""
     # attributs
-def __init__(self,arme,inventaire = []) :
-
-        self.arme = arme
-        self.inventaire = inventaire
+def __init__(self,name,descriptiton,pv,defense,initiative,attaque,type_de_degat,etat,arme,inventaire) :
+    super().__init__(name,descriptiton,pv,defense,initiative,attaque,type_de_degat,etat)
+    self.arme = arme
+    self.inventaire = inventaire
 def afficher_les_caracteristiques(self) : 
-
-
-        print(f" {self.arme} , {self.inventaire}")
+    
+    return f" Nom : {self.name} ,desc : {self.description}, pv : {self.pv},,inv : {self.inventaire}"
 
 def liste_des_personnage(personnage = {}) :
+    
+    personnage = {"1.gojo","2.racnar","3.tanjiro","4.ichigo"}
 
-    personnage = { "1.gojo","2.racnar","3.tanjiro","4.ichigo"}
-
-gojo = personnage("artefact","lunette,sucette,portefeuille",)
-racnar = personnage("épée","boussole,vin,remède")
-tanjiro = personnage("katana,nezuko,boulette de riz")
-ichigo = personnage("zanpakto","")
+# gojo = personnage("artefact","lunette,sucette,portefeuille",)
+# racnar = personnage("épée","boussole,vin,remède")
+# tanjiro = personnage("katana,nezuko,boulette de riz")
+# ichigo = personnage("zanpakto","")
 
 
 class monstre(creature) : #classe monnstre 
@@ -82,3 +81,42 @@ class monstre(creature) : #classe monnstre
         
         self.resistance = resistance
     
+def choix_armes():
+    listam = ["lame maudite", "annulation d'attaque", "sabre", "katana"]
+    print("Veuillez choisir une arme parmi :")
+    for i, arme in enumerate(listam,1):  # Affiche la liste avec les indices commencant par un 
+        print(f"{i}. {arme}")
+
+    while True:
+        try:
+            choix = int(input("Entrez le numéro de l'arme : "))
+            if 1 <= choix <= len(listam):  # Vérifie que le choix est valide
+                print(f"Vous avez choisi : {listam[choix - 1]}")
+                break
+            else:
+                print("Veuillez entrer un numéro valide.")
+        except ValueError:
+            print("Veuillez entrer un nombre valide.")
+
+# Appeler la fonction
+# choix_armes()
+
+def liste_cible():
+
+    listcib = ["gojo","racnar","tanjiro","ichigo","dragon noir","lune supérieur","loki"]
+    print("veuillez choisir une cible:")
+    for x, cible in enumerate(listcib,1):
+        print(f"{x}.{cible}")
+    
+    while True:
+        try:
+            choix = int(input("Entrez le numéro de l'arme : "))
+            if 1 <= choix <= len(listcib):  # Vérifie que le choix est valide
+                print(f"Vous avez choisi : {listcib[choix - 1]}")
+                break
+            else:
+                print("Veuillez entrer un numéro valide.")
+        except ValueError:
+            print("Veuillez entrer un nombre valide.")
+liste_cible()
+        
